a7f53662938fd7c4edd114b16d9bf09a2581936f
kevinLee2015 <xx@xx.com>
2022-03-03 09:28:08 +0800

    no message

--- src/utils/themeStyle.js
+++ src/utils/themeStyle.js
@@ -25,6 +25,7 @@ const themeStyle = {
             light: " bg-blueGray-50 text-blueGray-500 border-blueGray-100 ",
             dark: " bg-lightBlue-800 text-lightBlue-300 border-lightBlue-700 ",
         },
+
 };
 
 export default themeStyle
