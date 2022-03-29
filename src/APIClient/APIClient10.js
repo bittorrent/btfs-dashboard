0f4adacd06a841ddcb1b442064ab9891fd618a68
kevinLee2015 <xx@xx.com>
2022-03-29 12:47:33 +0800

    no message

--- src/APIClient/APIClient10.js
+++ src/APIClient/APIClient10.js
@@ -180,7 +180,7 @@ class APIClient10 {
     }
 
     withdraw(amount) {
-        return this.request('/api/v1/vault/withdraw?arg=' + amount, {}, {
+        return this.request('/api/v1/vault/withdraw?arg=' + amount, {data:amount}, {
             headers: {
                 'Content-Type': 'text/plain'
             }
